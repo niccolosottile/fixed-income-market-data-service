3a9c1470aec3a57b8c21bff69494b0d29d8df9da
= <niccolosottile@outlook.com>
2025-05-27 15:33:08 +0100

    Minor refactoring

--- src/main/java/com/fixedincome/marketdata/config/RestTemplateConfig.java
+++ src/main/java/com/fixedincome/marketdata/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.fixedincome.marketdata.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,6 @@ public class RestTemplateConfig {
     
   private final FredApiProperties fredApiProperties;
 
-  @Autowired
   public RestTemplateConfig(FredApiProperties fredApiProperties) {
     this.fredApiProperties = fredApiProperties;
   }
@@ -21,8 +19,8 @@ public class RestTemplateConfig {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplateBuilder()
-      .setConnectTimeout(Duration.ofMillis(fredApiProperties.getConnectTimeout()))
-      .setReadTimeout(Duration.ofMillis(fredApiProperties.getReadTimeout()))
+      .connectTimeout(Duration.ofMillis(fredApiProperties.getConnectTimeout()))
+      .readTimeout(Duration.ofMillis(fredApiProperties.getReadTimeout()))
       .build();
   }
 }
